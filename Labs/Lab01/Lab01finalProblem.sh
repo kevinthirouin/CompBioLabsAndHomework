e5dc2108bc11798e9e93b8247d0f710056902d7f
kevinthirouin <kevin.thirouin@colorado.edu>
2020-02-04 04:24:03 +0000

    added comment on known problem with shell script

--- Labs/Lab01/Lab01finalProblem.sh
+++ Labs/Lab01/Lab01finalProblem.sh
@@ -1,3 +1,5 @@
+#This works if I copy/paste in one line at a time. I can't get the script to run in the shell.
+
 #isolate the column headers
 grep time PredPreyData.csv > header.csv
 #subset original data file to columns of interest
