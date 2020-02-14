382ba538dcd033f7798e7a1ad17ffe245d7b52c5
kevinthirouin <kevin.thirouin@colorado.edu>
2020-02-14 19:30:13 +0000

    finished question 1

---
+++ Labs/Lab05/Thirouin_Lab05.R
@@ -0,0 +1,14 @@
+
+##Part 1:
+
+#1) Create variable x, and if-else statement to check if value is larger than 5
+x <- 10
+
+if(x > 5) {
+  print("variable x is greater than 5")
+} else {
+  print("variable x is less than 5")
+}
+
+#2) 
+
