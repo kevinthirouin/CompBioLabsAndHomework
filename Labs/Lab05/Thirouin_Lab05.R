8b9055ea20f17f112f5da988ee8454d3b6049d2f
kevinthirouin <kevin.thirouin@colorado.edu>
2020-02-14 20:01:17 +0000

    successfully created for loop for question 2a

--- Labs/Lab05/Thirouin_Lab05.R
+++ Labs/Lab05/Thirouin_Lab05.R
@@ -10,5 +10,17 @@ if(x > 5) {
   print("variable x is less than 5")
 }
 
-#2) 
+#####################
 
+#2) Import ExampleData.csv file
+
+ExampleData <- read.csv(file.choose()) #Import ExampleData.csv file
+str(ExampleData) #check structure of data -- data frame of one variable
+DataVector <- ExampleData$x #move data into a vector for manipulation
+
+##2a) create a for loop to change every negative value in the DataVector to NA
+
+for(i in DataVector) {
+  if(DataVector[i] < 0)
+  
+}
