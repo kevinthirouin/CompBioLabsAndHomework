d353dd84a460ee0bdaa5c39e887be6827f146e1c
kevinthirouin <kevin.thirouin@colorado.edu>
2020-02-27 19:34:00 +0000

    complete lab04

--- Labs/Lab04/Thirouin_Lab4.R
+++ Labs/Lab04/Thirouin_Lab4.R
@@ -44,17 +44,21 @@ expected_pop #print results
 #4) Predicting values from the discrete-time logistic growth equation
 
 #Define parameters
-n <- seq(0, 12) #empty vector
-n[1] <- 2500
-K <- 10000
-r <- 0.8
+n <- seq(0, 12) #vector
+n[1] <- 2500 #population abundance at timestep 1
+K <- 10000 #population carrying capacity
+r <- 0.8 #intrinsic growth rate of population
 
-#loop
+#loop -- NOTE: for some reason, putting the print(i) or print(n) function
+#inside the for loop does not seem to output the vector in the correct format
+#so I moved the print functions outside the loop
+
 for(i in 2:length(n)) {
   n[i] <- n[i-1] + ( r * n[i-1] * ( K - n[i-1]) / K )
-  print(i)
-  print(n)
 }
+print(i)
+print(n)
+print(n[12])
 
 ###
 ##Part 2: Practice writing for loops and storing data in arrays
@@ -63,28 +67,60 @@ for(i in 2:length(n)) {
 #5) Practice basics of array indexing with for loops
 
 #5a)
-vec_18 <- rep(0, 18) #make vector of 18 zeros
+vec_18 <- rep(0 , 18) #make vector of 18 zeros
 vec_18 #print new vector in console
 
 #5b)
-for(i in seq(1,18)) {
-  vec_18 <- 
+for(i in seq(1 , 18)) {
+  vec_18[i] <- i * 3  #multiply the ith element of the vector by 3
 }
+print(vec_18)
+
+#5c)
+newVec1 <- rep(0 , 18)  #make new vector of 18 zeros
+newVec1[1] <- 1  #replace the first element of the vector with 1
+print(newVec1)
+
+#5d)
+
+#loop over newVec1 values, starting with the second value
+#add 1 and multiply the previous entry by 2, for
+#each iteration (for each value in newVec1 affected by the loop)
+
+for(i in 2:length(newVec1)) {
+  newVec1[i] <-  (newVec1[i-1] * 2) + 1
+}
+print(newVec1)
 
 
+#6) Fibonacci sequence
+
+#make a vector of 20 zeros to store Fibonacci numbers in
+fibVec <- rep(0, 20)
+print(fibVec)
+
+fibVec[2] <- 1
+
+for(i in 3:length(fibVec)) {
+  fibVec[i] <- (fibVec[i-2] + fibVec[i-1])
+}
+print(fibVec)
 
 
+#7) Storing values and plotting data from question 4
 
+#make 12 step time vector, empty storage vector for abundance, and define parameters
+time <- seq(1,12)
+abundance <- seq(1, 12) #vector of abundances
+abundance[1] <- 2500 #population abundance at timestep 1
+carryingCap <- 10000 #population carrying capacity
+growthRate <- 0.8 #intrinsic growth rate of population
 
+#loop
+for(i in 2:length(abundance)) {
+  abundance[i] <- abundance[i-1] + ( growthRate * abundance[i-1] * ( carryingCap - abundance[i-1]) / K )
+}
+print(abundance)
 
-
-
-
-
-
-
-
-
-
-
-
+#plot abundance over time
+plot(time, abundance)
